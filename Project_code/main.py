8b6872f17d25a52f177784ebef5a8a38649c8f81
Hanna Yashchuk <hannayashchuk8@gmail.com>
2024-11-14 21:47:59 +0100

    Interface

--- Project_code/main.py
+++ Project_code/main.py
@@ -7,21 +7,53 @@ import senantic_conformance_word
 import import_t
 import conformance_heatmap_vis
 import activity_interaction_network
-
-
-
-
-
-
-
-
-
+import tkinter as tk
 
 def petri_nets_visualization ():
-    raise NotImplementedError
+    print ("It is a petri net")
+    return None
 
-def performance_spectum_visualization ():
-    raise NotImplementedError
+def performance_spectrum_visualization ():
+    print("It is a performance spectrum")
+    return None
 
-def dottet_chart_visualization():
-    raise NotImplementedError
+def dotted_chart_visualization():
+    print("It is Dotted Chart")
+    return None
+
+
+
+
+root = tk.Tk()
+root.title ("Implementation of Process Mining Visualizations for Conformance Checking")
+root.geometry ("1000x600")
+
+def on_button_click(button_name):
+    if (button_name == "Petri Nets"):
+        petri_nets_visualization()
+
+    if (button_name == "Dotted Chart"):
+        dotted_chart_visualization()
+
+    if (button_name == "Performance Spectrum"):
+        performance_spectrum_visualization()
+
+    print(f"{button_name} clicked")
+
+
+buttons = []
+button_names = ["Upload data", "AIN", "Conformance Heatmap", "SEC",
+                "Semantic Conformance Word Cloud", "Petri Nets", "Performance Spectrum", "Dotted Chart"]
+
+for i, name in enumerate(button_names):
+    button = tk.Button(root, text=name, width=30, height=2,
+                       command=lambda name=name: on_button_click(name))
+    buttons.append(button)
+    button.grid(row=i//4, column=i%4, padx=10, pady=10)
+root.mainloop()
+
+
+
+
+
+
