d8242f402dc67f883e002e83e950b740e75ec44a
Maulik Jain <maulikjain98@yahoo.com>
2024-11-18 19:40:06 +0100

    have fun

--- Project_code/main.py
+++ Project_code/main.py
@@ -7,21 +7,36 @@ import senantic_conformance_word
 import import_t
 import conformance_heatmap_vis
 import activity_interaction_network
+import tkinter as tk
+import dotted_chart
+
+file_path = r"E:\Conformance-Checking-Visualizations\Project_code\receipt.csv"
+
+root = tk.Tk()
+root.title ("Implementation of Process Mining Visualizations for Conformance Checking")
+root.geometry ("1000x600")
+
+def on_button_click(button_name):
+    if (button_name == "Petri Nets"):
+        petri_nets_visualization()
+
+    if (button_name == "Dotted Chart"):
+        dotted_chart.generate_dotted_chart(import_t.import_csv(file_path))
+        
+
+    if (button_name == "Performance Spectrum"):
+        performance_spectrum_visualization()
+
+    print(f"{button_name} clicked")
 
 
+buttons = []
+button_names = ["Upload data", "AIN", "Conformance Heatmap", "SEC",
+                "Semantic Conformance Word Cloud", "Petri Nets", "Performance Spectrum", "Dotted Chart"]
 
-
-import_t.import_csv("E:\Conformance-Checking-Visualizations\Project_code\running-example.csv")
-
-
-
-
-
-def petri_nets_visualization ():
-    raise NotImplementedError
-
-def performance_spectum_visualization ():
-    raise NotImplementedError
-
-def dottet_chart_visualization():
-    raise NotImplementedError
+for i, name in enumerate(button_names):
+    button = tk.Button(root, text=name, width=30, height=2,
+                       command=lambda name=name: on_button_click(name))
+    buttons.append(button)
+    button.grid(row=i//4, column=i%4, padx=10, pady=10)
+root.mainloop()
