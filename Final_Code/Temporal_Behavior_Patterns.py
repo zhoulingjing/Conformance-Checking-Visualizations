ea55e0e0e204021887622b075b1a8c1e542d9075
Maulik Jain <maulikjain98@yahoo.com>
2024-11-27 13:14:54 +0100

    tbp_code

---
+++ Final_Code/Temporal_Behavior_Patterns.py
@@ -0,0 +1,145 @@
+import pm4py
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+from matplotlib.pyplot import cm
+import random
+import colorsys
+import numpy as np
+
+def calc_frequency(event_log):
+    frequencies = {}
+    for activity in event_log['concept:name']:
+        if activity in frequencies.keys():
+            frequencies[activity] += 1
+        else:
+            frequencies.update({activity: 1})
+    return frequencies
+
+def get_unique_activities(event_log):
+    return list(set(event_log['concept:name']))
+
+def get_unique_time(time):
+    times = []
+    for i in time:
+        if i in times:
+            continue
+        else:
+            times.append(i)
+    return times
+
+def sort_time(event_log, time_format):
+    match time_format:
+        case 'months':
+            months = {
+                'Jan': 0,
+                'Feb': 1,
+                'Mar': 2,
+                'Apr': 3,
+                'May': 4,
+                'Jun': 5,
+                'Jul': 6,
+                'Aug': 7,
+                'Sep': 8,
+                'Oct': 9,
+                'Nov': 10,
+                'Dec': 11,
+            }
+            return sorted(event_log['time:timestamp'], key=months.get)
+        case 'days':
+            days = {
+                'Monday': 1,
+                'Tuesday': 2,
+                'Wednesday': 3,
+                'Thursday': 4,
+                'Friday': 5,
+                'Saturday': 6,
+                'Sunday': 7,
+            }
+            return sorted(event_log['time:timestamp'], key=days.get)
+
+        case 'hours':
+            return sorted(event_log['time:timestamp'], key=lambda x: datetime.strptime(x, "%H:%M").time())
+
+        case 'years':
+            return sorted(event_log['time:timestamp'], key=int)
+
+def process_timestamp(event_log, time_format):
+    event_log['time:timestamp'] = pd.to_datetime(event_log['time:timestamp'])
+    match time_format:
+        case 'hours':
+            event_log['time:timestamp'] = event_log['time:timestamp'].dt.strftime("%H:00")
+
+        case 'days':
+            event_log['time:timestamp'] = event_log['time:timestamp'].dt.strftime("%A")
+
+        case 'months':
+            event_log['time:timestamp'] = event_log['time:timestamp'].dt.strftime("%b")
+
+        case 'years':
+            event_log['time:timestamp'] = event_log['time:timestamp'].dt.strftime("%Y")
+    return event_log
+
+def match_activity(event_log, target_time):
+    matched_activities = event_log[event_log['time:timestamp'] == target_time]
+    frequencies = calc_frequency(matched_activities)
+    return frequencies
+
+def generate_color(index, total):
+    hue = index / total  # Evenly spaced hues
+    saturation = 0.8     # High saturation for vibrant colors
+    value = 0.9          # High brightness
+    return colorsys.hsv_to_rgb(hue, saturation, value)
+
+
+def generate_Temporal_Behavior_Chart(event_log, time_format):
+    event_log = process_timestamp(event_log, time_format)
+    figure, axs = plt.subplots(figsize=(12, 6))
+
+    unique_activities = get_unique_activities(event_log)
+    color_map = {activity: generate_color(i, len(unique_activities)) for i, activity in enumerate(unique_activities)}
+
+    sorted_time = sort_time(event_log, time_format)
+    unique_time = get_unique_time(sorted_time)
+    plotted_activities = []
+
+    # Collect frequency data for dotted lines
+    activity_frequency_over_time = {activity: [] for activity in unique_activities}
+
+    # Set seed for reproducibility of jitter
+    #np.random.seed(42)
+
+    for time in unique_time:
+        activities = match_activity(event_log, time)
+        x = [time]
+        for activity in unique_activities:
+            frequency = activities.get(activity, 1)
+            activity_frequency_over_time[activity].append(frequency)
+
+            # Add jitter to the points for visualization
+           # jitter = np.random.normal(0, 0.1)
+            y = frequency 
+
+            if activity not in plotted_activities:
+                plt.plot(x, y, 'o', color=color_map[activity], label=activity)
+                plotted_activities.append(activity)
+            else:
+                plt.plot(x, y, 'o', color=color_map[activity])
+
+    # Add dotted lines for each activity
+    for activity, frequencies in activity_frequency_over_time.items():
+        x_positions = range(len(unique_time))  # Map unique times to sequential indices
+        plt.plot(x_positions, frequencies, '--', color=color_map[activity], alpha=0.7)
+
+    # Configure the plot
+    plt.xticks(ticks=range(len(unique_time)), labels=unique_time, rotation=45)
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1), title="Activities")
+    plt.xlabel('Time')
+    plt.ylabel('Frequency')
+    plt.tight_layout()
+    plt.show()
+
+
+# Load event log and generate chart
+event_log = pm4py.read_xes('Final_Code/running-example.xes')
+generate_Temporal_Behavior_Chart(event_log, time_format='days')
