f8925a86c0b860a7f9091968fc43f0595d7fe943
Maulik Jain <maulikjain98@yahoo.com>
2024-12-02 21:41:54 +0100

    changes

--- Final_Code/Temporal_Behavior_Patterns.py
+++ Final_Code/Temporal_Behavior_Patterns.py
@@ -3,10 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
 from matplotlib.pyplot import cm
-import random
 import colorsys
-import numpy as np
+import os
 
+#calculate frecuency of each activity
 def calc_frequency(event_log):
     frequencies = {}
     for activity in event_log['concept:name']:
@@ -16,9 +16,11 @@ def calc_frequency(event_log):
             frequencies.update({activity: 1})
     return frequencies
 
+#get all unique activities
 def get_unique_activities(event_log):
     return list(set(event_log['concept:name']))
 
+#get unique timestamps
 def get_unique_time(time):
     times = []
     for i in time:
@@ -28,6 +30,7 @@ def get_unique_time(time):
             times.append(i)
     return times
 
+#sort time in chronological order
 def sort_time(event_log, time_format):
     match time_format:
         case 'months':
@@ -64,12 +67,14 @@ def sort_time(event_log, time_format):
         case 'years':
             return sorted(event_log['time:timestamp'], key=int)
 
+#change timestamp based on the given time_format
 def process_timestamp(event_log, time_format):
     event_log['time:timestamp'] = pd.to_datetime(event_log['time:timestamp'])
     match time_format:
         case 'hours':
+            event_log['time:timestamp'] = event_log['time:timestamp'].dt.round('60min')
             event_log['time:timestamp'] = event_log['time:timestamp'].dt.strftime("%H:00")
-
+# 14:58 14pm
         case 'days':
             event_log['time:timestamp'] = event_log['time:timestamp'].dt.strftime("%A")
 
@@ -80,19 +85,34 @@ def process_timestamp(event_log, time_format):
             event_log['time:timestamp'] = event_log['time:timestamp'].dt.strftime("%Y")
     return event_log
 
+#for a given target_time give frequency of all activities that happen at that time
 def match_activity(event_log, target_time):
     matched_activities = event_log[event_log['time:timestamp'] == target_time]
     frequencies = calc_frequency(matched_activities)
     return frequencies
 
+#generate colors for different activities
 def generate_color(index, total):
     hue = index / total  # Evenly spaced hues
     saturation = 0.8     # High saturation for vibrant colors
     value = 0.9          # High brightness
     return colorsys.hsv_to_rgb(hue, saturation, value)
 
+#convert csv and xes to dataframe
+def convert_to_dataframe(file_path):
+    type = os.path.splitext(file_path)
+    if type[1] == '.xes':
+        event_log = pm4py.read_xes(file_path)
 
-def generate_Temporal_Behavior_Chart(event_log, time_format):
+
+    elif type[1] == '.csv':
+        event_log = pd.read_csv(file_path)
+        event_log = pm4py.format_dataframe(event_log, case_id='case:concept:name',activity_key='concept:name',timestamp_key='time:timestamp')
+
+    return event_log
+
+def generate_Temporal_Behavior_Chart(file_path, time_format):
+    event_log = convert_to_dataframe(file_path)
     event_log = process_timestamp(event_log, time_format)
     figure, axs = plt.subplots(figsize=(12, 6))
 
@@ -106,20 +126,13 @@ def generate_Temporal_Behavior_Chart(event_log, time_format):
     # Collect frequency data for dotted lines
     activity_frequency_over_time = {activity: [] for activity in unique_activities}
 
-    # Set seed for reproducibility of jitter
-    #np.random.seed(42)
-
     for time in unique_time:
         activities = match_activity(event_log, time)
         x = [time]
         for activity in unique_activities:
-            frequency = activities.get(activity, 1)
+            frequency = activities.get(activity, 0)
             activity_frequency_over_time[activity].append(frequency)
-
-            # Add jitter to the points for visualization
-           # jitter = np.random.normal(0, 0.1)
             y = frequency 
-
             if activity not in plotted_activities:
                 plt.plot(x, y, 'o', color=color_map[activity], label=activity)
                 plotted_activities.append(activity)
@@ -141,5 +154,4 @@ def generate_Temporal_Behavior_Chart(event_log, time_format):
 
 
 # Load event log and generate chart
-event_log = pm4py.read_xes('Final_Code/running-example.xes')
-generate_Temporal_Behavior_Chart(event_log, time_format='days')
+generate_Temporal_Behavior_Chart("Final_Code/running-example.xes", time_format='days')
--- Final_Code/conformance_heatmap_vis.py
+++ Final_Code/conformance_heatmap_vis.py
@@ -1,1 +1,99 @@
-#TO BE IMPLEMENTED
+import pm4py
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.model_selection import train_test_split
+def get_unique_variants(event_log):
+    traces = []
+    vars = pm4py.get_variants(event_log)
+    for trace in vars:
+        traces.append(trace)
+    return traces
+
+def abbreviate_activities(list_of_activities):
+    final_word = ''
+    for activity in list_of_activities:
+        # Split the activity into words
+        words = activity.split()
+        # Form abbreviation with full first word and first 3 characters of the second word (if available)
+        if len(words) > 1:
+            abbreviation = words[0] + words[1][:3].capitalize()
+        else:
+            abbreviation = words[0]  # Use the full single word if there's only one word
+        final_word += abbreviation + '-'
+    return final_word[:-1]  # Remove the trailing '-'
+
+
+# Function to generate conformance heatmap with concept drift over time
+def generate_conformance_heatmap_with_time(file_path, coloring='YlGnBu', pnml_path=None):
+    """
+    file_path: file path to the log file
+    coloring: colouring for the heatmap
+    pnml_path=path of the pnml model by default is None
+    
+    """
+    try:
+        # Reading the XES file
+        event_log = pm4py.read_xes(file_path)
+        # Step 1: Split the log into training and testing sets
+        case_ids = event_log['case:concept:name'].unique()  # split data into test and training data
+        train_ids, test_ids = train_test_split(case_ids, test_size=0.45, random_state=42)
+        train_log = event_log[event_log['case:concept:name'].isin(train_ids)]
+        test_log = event_log[event_log['case:concept:name'].isin(test_ids)]
+        traces = get_unique_variants(test_log)
+        if pnml_path==None:
+            model, im, fm = pm4py.discover_petri_net_inductive(train_log) 
+        else:
+            model, im, fm = pm4py.read_pnml(pnml_path)
+        # test_log = event_log
+
+        time_series_train = []
+        y_axis_labels = [] 
+        for trace in traces:
+            subset_trace = []
+            time_series_for_trace = []
+            # print(trace)
+            test_log_filtered = pm4py.filtering.filter_variants(test_log,[trace])
+            for activity in trace:
+                subset_trace.append(activity)
+                test_log_filtered2 = pm4py.filtering.filter_event_attribute_values(test_log_filtered,attribute_key= 'concept:name', values=subset_trace,level='event')
+                fitness = pm4py.fitness_token_based_replay(test_log_filtered2,model,im,fm)
+                conformance_score = fitness['average_trace_fitness']
+                time_series_for_trace.append(conformance_score)
+                # print(conformance_score)
+            y_axis=abbreviate_activities(subset_trace)
+            print(y_axis)
+            y_axis_labels.append(y_axis)
+            time_series_train.append(time_series_for_trace)
+
+        # Convert time series lists to DataFrames for easy visualization
+        time_df_train = pd.DataFrame(time_series_train)
+
+        # Now generate the heatmap for training and testing logs
+        plt.figure(figsize=(12, 8))
+
+        
+
+        # Plot for training log
+        sns.heatmap(time_df_train, annot=False, cmap=coloring, cbar=True, xticklabels=True, yticklabels=y_axis_labels)
+        plt.title("Conformance Heatmap")
+        plt.xlabel("Steps in Trace")
+        plt.ylabel("Trace ID")
+
+        num_columns = time_df_train.shape[1]
+        plt.xticks(ticks=np.arange(num_columns) + 0.5, labels=np.arange(1, num_columns + 1))
+
+
+        # Display the heatmap
+        plt.tight_layout()
+        plt.show()
+        # plt.show()
+        plt.savefig('./Final_code/chcd.png')
+
+
+    except Exception as e:
+        print(f"Error generating conformance heatmap: {e}")
+
+# Example usage:
+generate_conformance_heatmap_with_time('Final_Code/running-example.xes',)
